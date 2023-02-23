d1f5347806b42726d005cecedb64f110ff3e6163
Luke Chamberland <luke.chamberland@shaw.ca>
2023-02-23 04:46:33 +0000

    removed function/ required it

--- play.js
+++ play.js
@@ -1,16 +1,4 @@
-const net = require("net");
+const {connect} = require('./client')
 
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 console.log("Connecting ...");
-connect();
+connect()
